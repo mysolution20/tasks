bd2ff2577dbb6df98840819325f9475b05fef34a
mysolution20 <62807367+mysolution20@users.noreply.github.com>
2020-07-08 18:08:18 +0200

    19.2. Tworzenie połączenia z bazą z poziomu aplikacji

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,20 +1,36 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")   //http://localhost:8080/v1/task/getTasks
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+
+/*      TaskDto taskDto1 = new TaskDto(1L, "Title No.1", "test_content No.1");
+        TaskDto taskDto2 = new TaskDto(2L, "Title No.2", "test_content No.2");
+        TaskDto taskDto3 = new TaskDto(3L, "Title No.3", "test_content No.3");
+        List<TaskDto> listTaskToDo = new ArrayList<>();
+        listTaskToDo.add(taskDto1);
+        listTaskToDo.add(taskDto2);
+        listTaskToDo.add(taskDto3);
+        return new ArrayList<>(listTaskToDo);*/
+
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")   // http://localhost:8080/v1/task/getTask
@@ -24,7 +40,6 @@ public class TaskController {
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask") // http://localhost:8080/v1/task/deleteTask
     public void deleteTask(Long taskId) {
-
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateTask")  // http://localhost:8080/v1/task/updateTask
@@ -34,6 +49,5 @@ public class TaskController {
 
     @RequestMapping(method = RequestMethod.POST, value = "createTask") // http://localhost:8080/v1/task/createTask
     public void createTask(TaskDto taskDto) {
-
     }
 }
