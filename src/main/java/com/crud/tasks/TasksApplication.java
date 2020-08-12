ad14a93e3c4bef886eb291778c8e09c58a122461
mysolution20 <62807367+mysolution20@users.noreply.github.com>
2020-08-12 15:37:17 +0200

    modul 23-  JAR and PostgreSQL

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,16 +2,16 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.boot.builder.SpringApplicationBuilder;                  //  <-- Tomkat
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;  //  <-- Tomkat
 
 @SpringBootApplication
-public class TasksApplication /*extends SpringBootServletInitializer*/ {
+public class TasksApplication /*extends SpringBootServletInitializer*/ {            /**  <-- Tomkat */
 	public static void main(String[] args) {
 	    SpringApplication.run(TasksApplication.class, args);
 	}
 
-/*    @Override
+/*    @Override                                                                      //  <-- Tomkat
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TasksApplication.class);
     }*/
