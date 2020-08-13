fc0228697e94fa8ec069e9bd74cba7ad153b825b
mysolution20 <62807367+mysolution20@users.noreply.github.com>
2020-08-13 17:03:50 +0200

    modul 23.2 Ograniczenie skanowania w Swaggerze

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
+
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/trello")
@@ -21,31 +22,34 @@ public class TrelloController {
     public List<TrelloBoardDto> getTrelloBoards() {
         return trelloClient.getTrelloBoards();
     }
-/**    public void getTrelloBoards() {
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println("Here we have trelloBoardDto: " + trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
-            System.out.println();
-            System.out.println("This board contains nested lists: < trelloList > ");
-            System.out.println();
-            trelloBoardDto.getLists().forEach(trelloList ->
-                    System.out.println( trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-
-        });
-
-        // 22.2 Zadanie: Ułożenie kodu - to review    //   StringUtils.isNotBlank  -->  import org.apache.commons.lang3.StringUtils;
-        Objects.requireNonNull(trelloBoards).stream()
-                .filter(e -> StringUtils.isNotBlank(e.getId()))              // -->   or general:  .filter(e-> e.getId() != null && !e.getId().isEmpty())
-                .filter(e -> e.getName() != null && !e.getName().isEmpty())
-                .filter(e -> e.getName().contains("Kodilla"))
-                .forEach(trelloBoardDto -> {
-                    System.out.println();
-                    System.out.println(" filtrowanie wyświetla tablice, które posiadają pola id i name ****");
-                    System.out.println();
-                    System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
-                });
-    }*/
+    /**
+     * public void getTrelloBoards() {
+     * List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+     * <p>
+     * trelloBoards.forEach(trelloBoardDto -> {
+     * System.out.println("Here we have trelloBoardDto: " + trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+     * System.out.println();
+     * System.out.println("This board contains nested lists: < trelloList > ");
+     * System.out.println();
+     * trelloBoardDto.getLists().forEach(trelloList ->
+     * System.out.println( trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+     * <p>
+     * });
+     * <p>
+     * // 22.2 Zadanie: Ułożenie kodu - to review    //   StringUtils.isNotBlank  -->  import org.apache.commons.lang3.StringUtils;
+     * Objects.requireNonNull(trelloBoards).stream()
+     * .filter(e -> StringUtils.isNotBlank(e.getId()))              // -->   or general:  .filter(e-> e.getId() != null && !e.getId().isEmpty())
+     * .filter(e -> e.getName() != null && !e.getName().isEmpty())
+     * .filter(e -> e.getName().contains("Kodilla"))
+     * .forEach(trelloBoardDto -> {
+     * System.out.println();
+     * System.out.println(" filtrowanie wyświetla tablice, które posiadają pola id i name ****");
+     * System.out.println();
+     * System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
+     * });
+     * }
+     */
 
     @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
     public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
